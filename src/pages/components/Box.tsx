d3d7b9909eb334c4e60d96c9242b450e20534c4d
ngdevelop-tech <aprajapati06@gmail.com>
2024-01-02 22:46:53 +0530

    Box drag and drop

--- src/pages/components/Box.tsx
+++ src/pages/components/Box.tsx
@@ -1,16 +1,23 @@
 import { useDrag } from 'react-dnd';
 import { ItemTypes } from '../../ItemType';
 import './styles/Box.scss';
+import * as classNames from 'classnames';
 
 export const Box = ({ name }) => {
-  const [{}, drag] = useDrag<{ name: string }, { name: string }>({
+  const [{isDragging}, drag] = useDrag<{ name: string }, { name: string }, {isDragging: boolean}>(()=>({
     type: ItemTypes.BOX,
     item: { name },
     end: (item, monitor) => {
       const dropResult = monitor.getDropResult();
+      if (item && dropResult) {
       alert(`You dropped ${item.name} into ${dropResult.name}`);
+      }
     },
-  });
+    collect: (monitor)=> ({
+      isDragging: monitor.isDragging(),
+      handlerId: monitor.getHandlerId()
+    })
+  }));
 
-  return <div className="box">{name}</div>;
+  return <div className={ classNames('box', { 'box--dragging': isDragging })} ref={drag}>{name}</div>;
 };
--- src/pages/components/Dustbin.tsx
+++ src/pages/components/Dustbin.tsx
@@ -1,5 +1,21 @@
+import { useDrop } from 'react-dnd';
 import './styles/Dustbin.scss';
+import { ItemTypes } from '../../ItemType';
+import * as classNames from 'classnames';
 
 export const Dustbin = () => {
-  return <div className="dustbin">Drag box here</div>;
+  const [{canDrop, isOver}, drop] = useDrop(()=>({
+    accept: ItemTypes.BOX,
+    drop: (item, monitor)=> {
+      console.log(">> Dustbin: (drop) : ", {item, monitor})
+      return {name: 'Dustbin'}
+    },
+    collect: (monitor)=> ({
+      isOver: monitor.isOver(),
+      canDrop: monitor.canDrop()
+    })
+  }))
+
+
+  return <div className={classNames('dustbin', {'dustbin--is-over': isOver, 'dustbin--can-drop': canDrop})} ref={drop}>Drag box here</div>;
 };
